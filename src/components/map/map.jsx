b5ccae8be25659cb2d5381c5be314ca55a114b39
visionjain <visionjain118@gmail.com>
2023-08-20 03:36:52 +0530

    responsive fix

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -8,23 +8,23 @@ import { BiUser } from 'react-icons/bi';
 const Map = () => {
   const tabItems = [
     {
-      icon: <ImStatsBars className="w-40 h-10" />,
+      icon: <ImStatsBars className="w-10 h-10 md:w-40 md:h-10" />,
       url: '/stats',
     },
     {
-      icon: <AiOutlineCamera className="w-40 h-10" />,
+      icon: <AiOutlineCamera className="w-10 h-10 md:w-40 md:h-10" />,
       url: '/scan',
     },
     {
-      icon: <BsMap className="w-40 h-10" />,
+      icon: <BsMap className="w-10 h-10 md:w-40 md:h-10" />,
       url: '/map',
     },
     {
-      icon: <PiMedalLight className="w-40 h-10" />,
+      icon: <PiMedalLight className="w-10 h-10 md:w-40 md:h-10" />,
       url: '/medal',
     },
     {
-      icon: <BiUser className="w-40 h-10" />,
+      icon: <BiUser className="w-10 h-10 md:w-40 md:h-10" />,
       url: '/profile',
     },
   ];
@@ -34,7 +34,7 @@ const Map = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
       <div className="max-w-screen-xl mx-auto px-4 md:px-8 text-center py-2">
-        <ul role="tablist" className="flex items-center justify-center gap-x-6 overflow-x-auto text-sm">
+        <ul role="tablist" className="flex items-center justify-center gap-x-2 md:gap-x-6 overflow-x-auto text-sm">
           {tabItems.map((item, idx) => (
             <li
               key={idx}
