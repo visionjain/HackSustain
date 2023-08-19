147c81c01ce92cebb6728fcb03648ff4db25ffb0
visionjain <visionjain118@gmail.com>
2023-08-19 21:19:05 +0530

    inital

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '@/components/login/login'
+const Page = () => {
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+import Signup from '@/components/sign up/signup'
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default signup
